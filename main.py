5f87ea34193964f8c81a4557969929369c5fe848
tingyz <2847035016@qq.com>
2025-04-04 14:49:12 +0800

    sub func

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ def add(a, b):
     return a + b
 
 
+def sub(a, b):
+    return a - b
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
